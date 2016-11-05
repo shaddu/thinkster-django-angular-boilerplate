fe9f69521f20ce0f2e0c38264a4f1630ac7235df
shad <Shad@Shadabs-MacBook-Air.local>
2016-11-05 17:33:57 +0530

    Registration Part
    Added Angular service to register new user

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,9 +1,17 @@
-from django.conf.urls import patterns, url
-
+from rest_framework_nested import routers
+from authentication.views import AccountViewSet
+from django.conf.urls import patterns
+from django.conf.urls import include, url 
 from thinkster_django_angular_boilerplate.views import IndexView
 
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
+
 urlpatterns = patterns(
-    '',
+     '',
+    # ... URLs
+    url(r'^api/v1/', include(router.urls)),
 
     url('^.*$', IndexView.as_view(), name='index'),
 )
