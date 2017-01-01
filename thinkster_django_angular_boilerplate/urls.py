26d021fcf631744c019e9cc025f53c7c38593da6
shad <Shad@Shadabs-MacBook-Air.local>
2017-01-02 00:44:30 +0530

    post model

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -3,11 +3,17 @@ from authentication.views import AccountViewSet, LoginView, LogoutView
 from django.conf.urls import patterns
 from django.conf.urls import include, url 
 from thinkster_django_angular_boilerplate.views import IndexView
-
-
+from posts.views import AccountPostsViewSet, PostViewSet
 
 router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
+router.register(r'posts', PostViewSet)
+
+accounts_router = routers.NestedSimpleRouter(
+    router, r'accounts', lookup='account'
+)
+accounts_router.register(r'posts', AccountPostsViewSet)
+
 
 urlpatterns = patterns(
      '',
@@ -15,6 +21,7 @@ urlpatterns = patterns(
     url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
     url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),    
     url(r'^api/v1/', include(router.urls)),
+    url(r'^api/v1/', include(accounts_router.urls)),
     url('^.*$', IndexView.as_view(), name='index'),
 
 )
